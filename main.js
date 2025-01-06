d39bd200e0402cf6f033bc51ebc854d5ac06f12a
Dip3102001 <dip3102001@gmail.com>
2025-01-06 09:06:11 -0500

    Add : adding collision mechnism and restrict sprite moment...

--- main.js
+++ main.js
@@ -8,13 +8,23 @@ const spriteImage = new Image();
 spriteImage.src = './SpriteMovement/down.png';
 
 
-let initX = -350;
-let initY = -450;
+let initX = -336;
+let initY = -432;
 
 
 const spriteX = 0;
 const spriteY = 0;
 const spriteSpeed = 5;
+
+
+
+const collision2D = [];
+
+const num_cols = 70;
+
+for(let i = 0; i < collision.length;i += num_cols)
+    collision2D.push(collision.slice(i, i + num_cols));
+
 
 gameImage.onload = function(){
     ctx.drawImage(gameImage, initX, initY);    
@@ -24,18 +34,16 @@ const spriteDimension = {};
 
 spriteImage.onload = function(){
 
-    spriteDimension.x = 320;
-    spriteDimension.y = 450;
+    spriteDimension.x = 336;
+    spriteDimension.y = 480;
 
-    spriteDimension.spriteSpeed = 5;
+    spriteDimension.spriteSpeed = 3;
 
     spriteDimension.imageX = spriteImage.height/2;
     spriteDimension.imageY = spriteImage.width/2;
 
     spriteDimension.originalWidth = spriteImage.width;
     spriteDimension.originalHeight = spriteImage.height;
-
-    console.log(spriteDimension.imageX+" "+spriteDimension.imageY);
 
     spriteDimension.scaleRatio = 4;
 
@@ -90,7 +98,6 @@ function drawSprite(){
     spriteDimension.imageX = i * spriteDimension.originalWidth/2;
     spriteDimension.imageY = j * spriteDimension.originalHeight/2;
 
-    // console.log(spriteDimension.imageX+" "+spriteDimension.imageY);
 
     ctx.drawImage(spriteImage,
         spriteDimension.imageX,spriteDimension.imageY,
@@ -102,23 +109,71 @@ function drawSprite(){
     );
 }
 
-function gameLoop() {
+function convertPixelToGird(initX, initY){
+    return {x: Math.floor(-initX/48), y: Math.floor(-initY/48)};
+}
+
+
+function isLegalMove(){
+    let isLegal = false;
+
+    const xx = 7+1;
+    const yy = 10+1;
+
+
+    const{x,y} = convertPixelToGird(initX, initY);
+    console.log((x+xx)+" "+(y+yy));
+
     if(keys.ArrowUp){
         initY += spriteDimension.spriteSpeed;
-        spriteDimension.direction = "up";
-        spriteDimension.movementCount++;
+        const{x,y} = convertPixelToGird(initX, initY);
+        isLegal = (collision2D[y+yy][x+xx] == 0); 
+        initY -= spriteDimension.spriteSpeed;
     }else if(keys.ArrowDown){
         initY -= spriteDimension.spriteSpeed;
-        spriteDimension.direction = "down";
-        spriteDimension.movementCount++;
+        const{x,y} = convertPixelToGird(initX, initY);
+        isLegal = (collision2D[y+yy][x+xx] == 0); 
+        initY += spriteDimension.spriteSpeed;
     }else if(keys.ArrowLeft){
         initX += spriteDimension.spriteSpeed;
-        spriteDimension.direction = "left";
-        spriteDimension.movementCount++;
+        const{x,y} = convertPixelToGird(initX, initY);
+        isLegal = (collision2D[y+yy][x+xx] == 0);
+        initX -= spriteDimension.spriteSpeed;
     }else if(keys.ArrowRight){
         initX -= spriteDimension.spriteSpeed;
-        spriteDimension.direction = "right";
-        spriteDimension.movementCount++;
+        const{x,y} = convertPixelToGird(initX, initY);
+        isLegal = (collision2D[y+yy][x+xx] == 0);
+        initX += spriteDimension.spriteSpeed;
+    }
+
+    return isLegal;
+}
+
+function gameLoop() {
+    if(keys.ArrowUp){
+        if(isLegalMove()){
+            initY += spriteDimension.spriteSpeed;
+            spriteDimension.direction = "up";
+            spriteDimension.movementCount++;
+        }
+    }else if(keys.ArrowDown){
+        if(isLegalMove()){
+            initY -= spriteDimension.spriteSpeed;
+            spriteDimension.direction = "down";
+            spriteDimension.movementCount++;
+        }
+    }else if(keys.ArrowLeft){
+        if(isLegalMove()){
+            initX += spriteDimension.spriteSpeed;
+            spriteDimension.direction = "left";
+            spriteDimension.movementCount++;
+        }
+    }else if(keys.ArrowRight){
+        if(isLegalMove()){
+            initX -= spriteDimension.spriteSpeed;
+            spriteDimension.direction = "right";
+            spriteDimension.movementCount++;
+        }
     } 
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -128,7 +183,4 @@ function gameLoop() {
     requestAnimationFrame(gameLoop);
 }
 
-
 gameLoop();
-
-
